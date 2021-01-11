dd588014124008e26d70c78213696150dc537761
jess-breda <jbreda19@gmail.com>
2021-01-11 15:37:12 -0500

    up and running on single files

--- find_wireless_sess_J.m
+++ find_wireless_sess_J.m
@@ -129,128 +129,151 @@ end
 sessiondate = ['20' date_str([1 2]) '-' date_str([3 4]) ...
     '-' date_str([5 6])];
 
-%% STOPPED HERE
-% load ttls from wireless
+%% TTLS
+
+%--- ephys ---
 dio     = readTrodesExtractedDataFile(dio_file);
 c       = dio.fields(1).data;
 b       = dio.fields(2).data;
-ttls    = double(c(b==1))/fs; % this is ttls in seconds
+ttls    = double(c(b==1))/fs; % convert ttls into seconds
 
-inc     = 0;
 
-ndays = 60;
 
-for ss = 0:(ndays-1);
+% --- behavior ---
+inc     = 0; % make a counter
+ndays_back = 3; % how many days back do you want to search in bdata for a match?
+
+for ss = 0:(ndays_back-1);
+    % YY-MM-DD to YYMMDD
     this_datestr = datestr(datenum(sessiondate) - ss,'yymmdd');
     
-    for rr = 1:length(rat_name)
-        fntemp      = fullfile(behav_dir, rat_name{rr}, ['data_*' this_datestr '*.mat']);
-        ratfiles    = dir(fntemp);
-        if isempty(ratfiles)
-            fprintf(['couldn''t find a match for ' strrep(fntemp,'\','\\')])
-            continue
-        end
-        for ff=1:length(ratfiles)
-            inc             = inc +1;
-            rats{inc}       = rat_name{rr};
-            this_behav_name = ratfiles(ff).name;
-            this_behav_path = fullfile(ratfiles(ff).folder, this_behav_name);
-            filename{inc}   = this_behav_name;
-            behav_path{inc} = this_behav_path;
-            warning('off')
-            load(this_behav_path, 'saved_history');
-            warning('on')
-            parsed_events   = saved_history.ProtocolsSection_parsed_events;
-            % load behavior TTLS for alignment with ephys
-            ttls_fsm = nan(1,length(parsed_events));
-            for i=1:length(parsed_events)
-                if(isfield(parsed_events{i}.states,'sending_trialnum') && ...
-                        length(parsed_events{i}.states.sending_trialnum)>0 )
-                    ttls_fsm(i) = parsed_events{i}.states.sending_trialnum(1);
-                else
-                    ttls_fsm(i) = NaN;
-                end
-            end
-            ttls_fsm=ttls_fsm';
-            
-            % get interpulse interval of ttls
-            trode_gaps  = diff(ttls);
-            fsm_gaps    = diff(ttls_fsm);
-            
-            % look only at ttls delivered 5-10 seconds apart
-            trode_gaps_valid    = find(trode_gaps > 5 & trode_gaps < 10);
-            % look for correspondance between the phys and behav sess ITIs
-            ttls_fsmall=[];
-            ttl_trodesall=[];
-            for i=1:length(trode_gaps_valid)
-                fsm_trodes_diffs=find(abs(fsm_gaps - trode_gaps(trode_gaps_valid(i))) < 0.05);
-                for k=1:length(fsm_trodes_diffs)
-                    indb=fsm_trodes_diffs(k);
-                    if (indb+5)>length(fsm_gaps)
-                        continue
-                    end
-                    if (trode_gaps_valid(i)+5)>length(trode_gaps)
-                        continue
-                    end
-                    vec1    = trode_gaps(trode_gaps_valid(i)+(0:5));
-                    vec2    = fsm_gaps(indb+(0:5));
-                    
-                    %if one of the distances is too long (timeout), abort
-                    if(max(abs(diff(vec1)))>30)
-                        continue
-                    end
-                    
-                    if(norm(vec1-vec2)>.1)
-                        continue
-                    else
-                        ttls(trode_gaps_valid(i));
-                        ttl_trodesall=[ttl_trodesall; ttls(trode_gaps_valid(i)+(0:5))];
-                        ttls_fsmall=[ttls_fsmall; ttls_fsm(indb+(0:5))];
-                    end
-                end
-            end
-            
-            rt{inc}     = polyfit(ttl_trodesall,ttls_fsmall,1);
-            
-            ttl_trode_fsm = ttl_trodesall*rt{inc}(1)+rt{inc}(2);
-            
-            if(isempty(ttl_trode_fsm))
-                max_residual(inc)   = 100;
-                totaldur(inc)       = 0;
+    % look for behavior files on this date & break if there are none
+    fntemp      = fullfile(behav_dir, rat_name, ['data_*' this_datestr '*.mat']); 
+    ratfiles    = dir(fntemp);
+    if isempty(ratfiles)
+        fprintf(['couldn''t find a match for ' strrep(fntemp,'\','\\'), 'you could try a larger ndays_back'])
+        continue
+    end
+    
+    % iterate over each file, pull info & load it
+    for nfile=1:length(ratfiles)
+        inc             = inc +1;
+        this_behav_name = ratfiles(nfile).name;
+        this_behav_path = fullfile(ratfiles(nfile).folder, this_behav_name);
+        filename{inc}   = this_behav_name;
+        behav_path{inc} = this_behav_path;
+        
+        warning('off')
+        load(this_behav_path, 'saved_history');
+        warning('on')
+        
+        % pull out events from fsm
+        parsed_events   = saved_history.ProtocolsSection_parsed_events;
+        
+        % load behavior TTLS by iterating over each event (trial) and 
+        % getting the ttl pulse time for when trial number was sent
+        ttls_fsm = nan(1,length(parsed_events)); 
+        for i=1:length(parsed_events)
+            if(isfield(parsed_events{i}.states,'sending_trialnum') && ...
+                    length(parsed_events{i}.states.sending_trialnum)>0 )
+                %only need the "on" time
+                ttls_fsm(i) = parsed_events{i}.states.sending_trialnum(1); 
             else
-                max_residual(inc)   = max(abs(ttl_trode_fsm-ttls_fsmall));
-                totaldur(inc)       = (max(ttls_fsmall)-min(ttls_fsmall))/60;
+                ttls_fsm(i) = NaN; % if wasn't new trial, keep at NaN
             end
+        end
+   
+        % --- behavior & ephys ----
+        % get interpulse interval of ttls
+        ttls_fsm=ttls_fsm';
+        trode_gaps  = diff(ttls);
+        fsm_gaps    = diff(ttls_fsm);
+
+        % look only at ttls delivered 5-10 seconds apart in trodes
+        trode_gaps_valid    = find(trode_gaps > 5 & trode_gaps < 10);
+        
+        % look for correspondance between the phys and behav sess ITIs
+        ttls_fsmall=[];
+        ttl_trodesall=[];
+        for i=1:length(trode_gaps_valid)
+            fsm_trodes_diffs=find(abs(fsm_gaps - trode_gaps(trode_gaps_valid(i))) < 0.05);
+            for k=1:length(fsm_trodes_diffs)
+                indb=fsm_trodes_diffs(k);
+                if (indb+5)>length(fsm_gaps)
+                    continue
+                end
+                if (trode_gaps_valid(i)+5)>length(trode_gaps)
+                    continue
+                end
+                vec1    = trode_gaps(trode_gaps_valid(i)+(0:5));
+                vec2    = fsm_gaps(indb+(0:5));
+
+                %if one of the distances is too long (timeout), abort
+                if(max(abs(diff(vec1)))>30)
+                    continue
+                end
+
+                if(norm(vec1-vec2)>.1)
+                    continue
+                else
+                    ttls(trode_gaps_valid(i));
+                    ttl_trodesall=[ttl_trodesall; ttls(trode_gaps_valid(i)+(0:5))];
+                    ttls_fsmall=[ttls_fsmall; ttls_fsm(indb+(0:5))];
+                end
+            end
+        end
+        
+        %run a linear regression on the trodes & fsm ttls
+        warning('off')
+        rt{inc}     = polyfit(ttl_trodesall,ttls_fsmall,1);
+        warning('on')
+        %save out the fx (y = mx + b)
+        ttl_trode_fsm = ttl_trodesall*rt{inc}(1)+rt{inc}(2);
+
+        if(isempty(ttl_trode_fsm))
+            max_residual(inc)   = 100;
+            totaldur(inc)       = 0;
+        else
+            max_residual(inc)   = max(abs(ttl_trode_fsm-ttls_fsmall));
+            totaldur(inc)       = (max(ttls_fsmall)-min(ttls_fsmall))/60; % in minutes
         end
     end
 end
 
-% figure it out if any of the behav sessions are acceptable matches
+%% SAVE OUT
+% figure it out if any of the behav sessions are acceptable matches by
+% having a small residual (ie ttls are similar ITIs) & lasting longer than 5 minutes
 match_ind  = find(max_residual<0.02 & totaldur>5);
 
 if(length(match_ind)<1)
     warning('no match!');
     save_path   = fullfile(mda_dir, 'no_ttl_match.mat');
+    
 elseif(length(match_ind)>1)
     disp(valore(match_ind))
     disp(filename(match_ind))
     warning('too many matches!')
     save_path   = fullfile(mda_dir, 'multiple_ttl_matches.mat');
-else
+    
+else 
     res.goodval     = max_residual(match_ind);
-    res.gooddur     = totaldur(match_ind);
-    res.spk2fsm_rt  = rt{match_ind};
-    res.spk2fsm_fn  = @(spk_ts) spk_ts.*res.spk2fsm_rt(1) + res.spk2fsm_rt(2);
-    res.goodpath    = behav_path{match_ind};
-    res.behfile     = filename{match_ind};
-    res.ratname     = rats{match_ind};
-    if bdata('connect') > 0
+    res.gooddur     = totaldur(match_ind);   % duration of session in minutes
+    res.spk2fsm_rt  = rt{match_ind};         % regression for spk time to behav time
+    
+    % fn below takes spike times as inputs and converts to behavior times
+    res.spk2fsm_fn  = @(spk_ts) spk_ts.*res.spk2fsm_rt(1) + res.spk2fsm_rt(2); 
+    
+    res.goodpath    = behav_path{match_ind}; % path to behavor session
+    res.behfile     = filename{match_ind};   % name of behavior session
+    res.ratname     = rat_name;
+    
+    if bdata('connect') > 0 % grab bdata info if connected
         sessid          =  bdata(['select sessid from sessions where data_file="'  res.behfile(1:end-4) '"']);
         res.sessid      = sessid;
     end
-    res.sessiondate = sessiondate;
+    res.sessiondate = sessiondate;           % date in YYYY-MM-DD
 end
 
-res.date_str    = date_str;
+res.date_str    = date_str;                  % date in YYMMDD
 res.save_path   = save_path;
 save(save_path, 'res', 'sess')
