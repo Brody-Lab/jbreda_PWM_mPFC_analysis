fe7e210d58586470ce3e11cbce6bf6bcffcbad40
jess-breda <jbreda19@gmail.com>
2021-02-01 14:06:48 -0500

    i/o & wrangling fx all set

--- utils.py
+++ utils.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 import scipy.io as spio
-# import matplotlib.pyplot as plt
-# import pandas as pd
-# import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 
 ## I/O
@@ -65,3 +65,117 @@ def load_nested_mat(filename):
 
     data = spio.loadmat(filename, struct_as_record=False, squeeze_me=True)
     return _check_vars(data)
+
+
+def load_session_info(beh_path, spks_path):
+    """
+    This function should be used to load behavior and spike data for a wireless tetrode
+    ephys session. It assumes that data is coming from matlab and structured as JRB has done.
+
+    inputs
+    ------
+    beh_path  : string, path to .mat file with behavior info
+    spks_path : string, path to .mat file with spiking info extracted from kilosort/raw data
+
+    returns
+    -------
+    beh_dict  : dict, with behavior .mat structure extracted
+    spks_dict : dict, with spikes .mat structures extracted
+    """
+    beh_dict = load_nested_mat(beh_path) # this function is custom to deal with nested structures
+    beh_dict = beh_dict["behS"]
+
+    spks_dict = spio.loadmat(spks_path, squeeze_me = True)
+    spks_dict = spks_dict['PWMspkS']
+
+    return beh_dict, spks_dict
+
+def make_beh_df(beh_dict):
+
+    """
+    Data wrangling function to take dictionary from load_session_info() and putting it into a
+    tidy dataframe for analysis
+
+    inputs
+    ------
+    beh_dict : dict, extracted .mat structure from load_session_info()
+
+    returns
+    -------
+    beh_df   : df (ntrials x items), tidy data frame with behavior information & some relabeling
+    """
+
+    # initialize data frame
+    beh_df = pd.DataFrame()
+
+    # rename previous side to be L/R
+    prev_side_adj = np.roll(beh_dict['prev_side'],1) # n-1 trial info
+    prev_side_adj = np.where(prev_side_adj == 114, 'RIGHT', 'LEFT' )
+    prev_side_adj[0] = 'NaN' # trial 0 doesn't have a previous
+
+    # turn hit info to strings
+    beh_df['hit_hist'] = beh_dict['hit_history']
+    beh_df['hit_hist'] = beh_df['hit_hist'].mask(beh_df['hit_hist'] == 1.0, "hit")
+    beh_df['hit_hist'] = beh_df['hit_hist'].mask(beh_df['hit_hist'] == 0.0, "miss")
+    beh_df['hit_hist'][beh_df['hit_hist'].isnull()] = "viol"
+
+    # get n_trial length items into df
+    beh_df['delay'] = beh_dict['delay']
+    beh_df['pair_hist'] = beh_dict['pair_history']
+    beh_df['correct_side'] = beh_dict['correct_side']
+    beh_df['prev_side'] = prev_side_adj
+    beh_df['aud1_sigma'] = beh_dict['aud1_sigma']
+    beh_df['aud2_sigma'] = beh_dict['aud2_sigma']
+
+    # extract parsed events/state machine info for each trial
+    parsed_events_dict = beh_dict["parsed_events"]
+
+    # initilize space
+    c_poke = np.zeros((len(parsed_events_dict)))
+    hit_state = np.zeros((len(parsed_events_dict)))
+    aud1_on = np.zeros((len(parsed_events_dict)))
+    aud1_off = np.zeros((len(parsed_events_dict)))
+    aud2_on = np.zeros((len(parsed_events_dict)))
+    aud2_off = np.zeros((len(parsed_events_dict)))
+
+    # iterate over items from state matrix
+    for trial in range(len(parsed_events_dict)):
+
+        # every trial has a center poke
+        c_poke[trial] = parsed_events_dict[trial]['states']['cp'][0]
+
+        # not all trials will have sound/hit time/etc, pull out info for non-violated
+        if beh_df['hit_hist'][trial] == 'viol':
+            hit_state[trial] = float("NaN")
+            aud1_on[trial]   = float("NaN")
+            aud1_off[trial]  = float("NaN")
+            aud2_on[trial]   = float("NaN")
+            aud2_off[trial]  = float("NaN")
+
+        elif beh_df['hit_hist'][trial] == 'hit':
+            hit_state[trial] = parsed_events_dict[trial]['states']['hit_state'][0]
+            aud1_on[trial]   = parsed_events_dict[trial]['waves']['stimAUD1'][0]
+            aud1_off[trial]  = parsed_events_dict[trial]['waves']['stimAUD1'][1]
+            aud2_on[trial]   = parsed_events_dict[trial]['waves']['stimAUD2'][0]
+            aud2_off[trial]  = parsed_events_dict[trial]['waves']['stimAUD2'][1]
+
+        elif beh_df['hit_hist'][trial] == 'miss':
+            hit_state[trial] = parsed_events_dict[trial]['states']['second_hit_state'][0]
+            aud1_on[trial]   = parsed_events_dict[trial]['waves']['stimAUD1'][0]
+            aud1_off[trial]  = parsed_events_dict[trial]['waves']['stimAUD1'][1]
+            aud2_on[trial]   = parsed_events_dict[trial]['waves']['stimAUD2'][0]
+            aud2_off[trial]  = parsed_events_dict[trial]['waves']['stimAUD2'][1]
+
+        else:
+            raise Exception('hit_hist doesn''t appear to have correct structure (hit, miss, viol)')
+
+
+    # add to df
+    beh_df['c_poke']    = c_poke
+    beh_df['hit_state'] = hit_state
+    beh_df['aud1_on']   = aud1_on
+    beh_df['aud1_off']  = aud1_off
+    beh_df['aud2_on']   = aud2_on
+    beh_df['aud2_off']  = aud2_off
+
+    return beh_df
