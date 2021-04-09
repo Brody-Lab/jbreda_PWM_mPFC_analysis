952ecfc0565174df4d1f873291ab703908592ea3
jess-breda <jbreda19@gmail.com>
2021-04-09 15:33:18 -0400

    end of day notes

--- plotting_utils.py
+++ plotting_utils.py
@@ -17,8 +17,64 @@ import scipy.io as spio
 # stored one repo up in my fork of Spykes
 from spykes.spykes.plot.neurovis import NeuroVis
 
+def split_spks_by_trial(spk_times, df):
+    """
+    Function that converts spikes for a whole trial (1d) into array where rows are trials
+    and columns are spk times within a trial for a neuron, added to dict with other trial level info
 
-def get_spike_counts(session_spk_times, session_trial_times, bin_size, mode, trial_len='same'):
+    inputs
+    ------
+    spk_times : arr-like, 1d containing spk times in seconds for a single neuron for a session
+    df        : df, containing behavior information for a trial
+
+    returns
+    -------
+    trial_spks_dict : dict, containing trial number (w/ respect to whole session), start and stop times
+                      for a trial and the spks occuring in a a trial in s
+    """
+
+    # initialize
+    trial_spks_dict = {}
+    trial_nums = []
+    trial_starts = []
+    trial_stops = []
+    trial_spks = []
+
+    # make counter because dataframe idx is not continous
+    # instead, it represents overall trial number with respect to start of session
+    itrial = 0
+
+    for idx, trial in df.iterrows():
+
+        # grab start time 100 ms before c poke
+        trial_start = trial['c_poke'] -0.001
+
+        # create end time so delays have same length
+        if trial['delay'] == 2:
+            trial_end = trial_start + 5.5
+        elif trial['delay'] == 4:
+            trial_end = trial_start + 7.7
+        elif trial['delay'] == 6:
+            trial_end = trial_start + 9.9
+        else:
+            print('Trial delay is not 2, 4 or 6 seconds- this is not correct')
+
+        # append trial info
+        trial_nums.append(idx)
+        trial_starts.append(trial_start)
+        trial_stops.append(trial_end)
+
+        # grab spike time within trial window & append them
+        trial_spks.append(spk_times[np.logical_and(spk_times >= trial_start, spk_times <= trial_end)])
+
+
+    trial_spks_dict['trial_nums'] = trial_nums
+    trial_spks_dict['trial_times'] = trial_starts, trial_stops
+    trial_spks_dict['trial_spks'] = trial_spks
+
+    return trial_spks_dict
+
+def get_spike_counts(trial_spks_dict, bin_size, mode, trial_len='same'):
 
     """
     Function that takes spike time information for a session split by trials and binarizes or counts the
@@ -26,9 +82,7 @@ def get_spike_counts(session_spk_times, session_trial_times, bin_size, mode, tri
 
     Inputs
     ------
-    session_spk_times   : list, list of list N trials long, each list contains spike times for the
-                          nth trial
-    session_trial_times : list, N trials X 2, start and stop times for each trial
+    trial_spks_dict     : dict, created by split_spks_by_trial()
     bin_size            : int, size of sliding bin to use in seconds
     mode                : str, whether binirze spike counts for a bin ('binary)', or count them ('count')
     trial_len           : str, whether to pack trials of different lenghts with 0s to make all the same
@@ -44,21 +98,22 @@ def get_spike_counts(session_spk_times, session_trial_times, bin_size, mode, tri
     only being counted as 1 spike. This appears to be quite rare on the scale of 2-5 bins on 25% of trials
     """
 
-    # initialize
+    # initialize & extract
     session_spk_counts = []
+    spks = trial_spks_dict['trial_spks']
 
     # iterate over spikes for each trial
-    for itrial, trial_spks in enumerate(tqdm(session_spk_times)):
+    for itrial, trial_spks in enumerate(tqdm(spks)):
 
         # grab start time
-        t_start = session_trial_times[itrial][0]
+        t_start = trial_spks_dict['trial_times'][0][itrial]
 
         # determine which end time to use given inputs
         # NOTE this assumes 6s delays are included so maximum t_len = 9.9 seconds
         if trial_len == 'same':
             t_end = t_start + 9.9
         elif trial_len == 'end':
-            t_end = session_trial_times[itrial][1]
+            t_end = trial_spks_dict['trial_times'][1][itrial]
         else:
             print('This is not a valid_trial length type')
 
@@ -73,8 +128,8 @@ def get_spike_counts(session_spk_times, session_trial_times, bin_size, mode, tri
         for ibin in range(n_bins):
             # for each of the spikes in the ith trial, do any fit in the ith bin?
             if mode == 'binary':
-                spike_in_ibin = np.logical_and(session_spk_times[itrial] >= (bin_centers[ibin] - half_bin),
-                                               session_spk_times[itrial] <= (bin_centers[ibin] + half_bin))
+                spike_in_ibin = np.logical_and(spks[itrial] >= (bin_centers[ibin] - half_bin),
+                                               spks[itrial] <= (bin_centers[ibin] + half_bin))
 
                 # if there is a spike in the bin, report it
                 if np.sum(spike_in_ibin) > 0:
@@ -82,14 +137,14 @@ def get_spike_counts(session_spk_times, session_trial_times, bin_size, mode, tri
 
             elif mode == 'count':
                 # for each of the spikes in the ith trial, how many fit into the ith bin?
-                n_spike_in_bin = np.sum(np.logical_and(session_spk_times[itrial] >= (bin_centers[ibin] - half_bin),
-                                                       session_spk_times[itrial] <= (bin_centers[ibin] + half_bin)))
+                n_spike_in_bin = np.sum(np.logical_and(spks[itrial] >= (bin_centers[ibin] - half_bin),
+                                                       spks[itrial] <= (bin_centers[ibin] + half_bin)))
 
                 trial_spk_counts[ibin] = n_spike_in_bin
 
         session_spk_counts.append(trial_spk_counts)
 
-    return session_spk_counts         
+    return session_spk_counts
 
 
 def initiate_neurons(spks_dict):
